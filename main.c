6263e1fc6c55ade6b19094fc1deacf93b7d9a0b8
Cathal Poon <cathal.poon@ucdconnect.ie>
2021-04-16 12:03:11 +0100

    Added description of the program to main.c

--- main.c
+++ main.c
@@ -1,3 +1,15 @@
+/*
+    Assignment 2
+
+    Written by: Cathal Poon
+    S/N: 20343243
+
+    A C program that manages a game of Othello (Reversi) between two players
+
+    For Module COMP10050
+    Assignment 2
+*/
+
 #include <stdio.h>
 
 int main() {
