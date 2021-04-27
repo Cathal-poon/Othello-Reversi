dfd916b7e20fd0d75df5ad44614fba5dbed44198
Cathal Poon <cathal.poon@ucdconnect.ie>
2021-04-27 19:28:00 +0100

    Added functionality to read in a name from the user and return a pointer to where it is stored.

--- players.c
+++ players.c
@@ -3,7 +3,10 @@
 //
 
 #include "players.h"
+#include <stdlib.h>
+#include <stdio.h>
 
+#define MAX_CHAR 10
 /*
 struct _player{
     char *name;
@@ -14,7 +17,15 @@ struct _player{
 */
 
 char *getName() {
+    char *name = (char*) malloc(MAX_CHAR * sizeof(char));
 
-
-    return NULL;
+    if (name == NULL){
+        printf("Error allocating memory");
+        return NULL;
+    } else {
+        printf("Please enter your player name:");
+        fflush(stdin);
+        scanf("%s",name);
+        return name;
+    }
 }
