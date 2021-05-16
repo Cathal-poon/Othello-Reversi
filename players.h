c0f10ca14713b4328880ad22df67d789a8f58423
Cathal Poon <cathal.poon@ucdconnect.ie>
2021-05-16 15:57:10 +0100

    Added comments to players.h describing the use of all the functions.

--- players.h
+++ players.h
@@ -14,7 +14,17 @@ typedef struct {
 
 
 void printPlayers(const player  * player1, const player * player2);
+/*
+ * Print out the players in the form
+ * Score: [Player1]([Colour]) [Score]:[Score] [Player2]([Colour])
+ */
 
 void initPlayers(player * player1, player * player2);
+/*
+ * Initialise the two players,
+ * read in names and assign colours
+ */
+
+
 
 #endif //ASSIGNMENT_2_PLAYERS_H
